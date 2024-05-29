60b0c6599dd84e4641372f689656a444bc65c8f1
Ignacio A. Molina <strocsdev@gmail.com>
2024-05-29 13:43:12 -0400

    add: og image and site description

--- src/data/SITE.ts
+++ src/data/SITE.ts
@@ -1,12 +1,15 @@
 export const SITE = {
-  title: 'En-Démico',
-  description: '',
+  title: 'Endémico | Patrimonio Natural y Cultural, Región de Coquimbo',
+  description: 'Rescate, registro y difusión del patrimonio cultural, la cultura ancestral y la artesanía local de la Región de Coquimbo.',
   favicon: {
     dark_mode: 'favicon_white.svg',
     light_mode: 'favicon_black.svg'
   },
   URL: 'https://en-demico.cl',
-  OG_image: '',
+  OG_image: {
+    dark_mode: 'og_image_dark.png',
+    light_mode: 'og_image_light.png'
+  },
   primary_colors: [
     '#6d8b4b',
     '#aabb51',
