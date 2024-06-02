65c1e525b7943f732161df77d801c7079c477723
Ignacio A. Molina <strocsdev@gmail.com>
2024-06-01 23:16:41 -0400

    add: pause video on slide change

--- src/services/swiperController.ts
+++ src/services/swiperController.ts
@@ -1,35 +1,34 @@
-import Swiper from "swiper";
-import { Navigation, Mousewheel } from "swiper/modules";
-import { placesList, header, logo, placesCardsList } from "@utils/domReferences";
-import { readLess } from "@utils/utilities";
+import Swiper from 'swiper'
+import { Navigation, Mousewheel } from 'swiper/modules'
+import { placesList, header, logo, placesCardsList } from '@utils/domReferences'
+import { readLess } from '@utils/utilities'
 
 // Initialize the vertical swiper
-export const verticalSwiper = new Swiper(".swiper-main", {
+export const verticalSwiper = new Swiper('.swiper-main', {
   modules: [Navigation, Mousewheel],
-  direction: "vertical",
+  direction: 'vertical',
   loop: true,
-  mousewheel: true,
-});
+  mousewheel: true
+})
 
-export const horizontalSwipers: { id: string, swiper: Swiper }[] = []
+export const horizontalSwipers: { id: string; swiper: Swiper }[] = []
 
-// Iterate through the placesList array
-placesList.forEach((place) => {
+placesList.forEach(async place => {
   // Create a new horizontal swiper for each place
   const swiper = new Swiper(`.swiper-${place.id}`, {
-    slidesPerView: "auto",
+    slidesPerView: 'auto',
     on: {
-      slideChange: (e) => {
+      slideChange: e => {
         if (e.realIndex === 1) {
-          header.style.opacity = "0";
-          header.style.pointerEvents = "none";
+          header.style.opacity = '0'
+          header.style.pointerEvents = 'none'
         } else {
-          header.style.opacity = "1";
-          header.style.pointerEvents = "auto";
+          header.style.opacity = '1'
+          header.style.pointerEvents = 'auto'
         }
       }
     }
-  });
+  })
 
   horizontalSwipers.push({ id: place.id, swiper })
 
@@ -44,66 +43,67 @@ placesList.forEach((place) => {
     } else {
       swiper.slidePrev()
     }
-  });
-});
-
-// Reset horizontal swipers when vertical swiper changes slides
-verticalSwiper.on('slideChange', (e) => {
-  if (window.innerWidth < window.innerHeight) return
-
-  horizontalSwipers.forEach(swiper => {
-    swiper.swiper.slideTo(0, 800)
   })
 })
 
-// Reset read less when vertical swiper changes slides
-verticalSwiper.on('slideChange', () => {
-  placesList.forEach(place => {
-    const btn = place.querySelector(".read-more-btn") as HTMLButtonElement;
-    if (!btn.getAttribute('data-isActive')) return
-
-    const image = place.querySelector(".place-img-mobile") as HTMLImageElement;
-    const video = place.querySelector(".place-video") as HTMLVideoElement;
-    const text = place.querySelector(
-      ".place-description"
-    ) as HTMLParagraphElement;
-
-    const minHeight = text.style.getPropertyValue('--min-height')
-
-    readLess({
-      btn, image, video, text, minHeight
+// Slide change events
+verticalSwiper.on('slideChange', e => {
+  // Reset horizontal swipers
+  if (window.innerWidth > window.innerHeight) {
+    horizontalSwipers.forEach(swiper => {
+      swiper.swiper.slideTo(0, 800)
     })
+  }
+
+  // Toggle header opacity
+  if (e.realIndex === 0) {
+    header.style.opacity = '0'
+    header.style.pointerEvents = 'none'
+  } else {
+    header.style.opacity = '1'
+    header.style.pointerEvents = 'auto'
+  }
+
+  // Reset read less
+  placesList.forEach(async place => {
+    const btn = place.querySelector('.read-more-btn') as HTMLButtonElement
+    const video = place.querySelector('.place-video') as any
+
+    if (btn.getAttribute('data-isActive')) {
+      const image = place.querySelector('.place-img-mobile') as HTMLImageElement
+      const text = place.querySelector(
+        '.place-description'
+      ) as HTMLParagraphElement
+
+      // Read less on slide change
+      const minHeight = text.style.getPropertyValue('--min-height')
+      readLess({
+        btn,
+        image,
+        video,
+        text,
+        minHeight
+      })
+    }
+
+    if (!!video.querySelector('iframe')) {
+      const player = await video.getYTPlayer()
+      if (player.getPlayerState() === 1) {
+        player.pauseVideo()
+      }
+    }
   })
 })
-
-
-verticalSwiper.on("slideChange", (e) => {
-  if (e.realIndex === 0) {
-    header.style.opacity = "0";
-    header.style.pointerEvents = "none";
-  } else {
-    header.style.opacity = "1";
-    header.style.pointerEvents = "auto";
-  }
-});
 
 // Add a click event listener to the logo element
-logo.addEventListener("click", () => {
+logo.addEventListener('click', () => {
   // When the logo is clicked, navigate the carousel to the first slide
-  verticalSwiper.slideToLoop(0, 0, false);
-});
+  verticalSwiper.slideToLoop(0, 0, false)
+})
 
-
-
+// Add a goto event listener to each place card
 placesCardsList.forEach((card, i) =>
-  card.addEventListener("click", () => {
-    verticalSwiper.slideToLoop(i + 1, 0, false);
+  card.addEventListener('click', () => {
+    verticalSwiper.slideToLoop(i + 1, 0, false)
   })
-);
-
-
-
-// TODO: Open image viewer on click
-// TODO: Reset image viewer on slide change
-// TODO: pause video on slide change 
-
+)
--- src/utils/utilities.ts
+++ src/utils/utilities.ts
@@ -1,45 +1,50 @@
 interface ReadMore {
-  image: HTMLImageElement;
-  text: HTMLParagraphElement;
-  btn: HTMLButtonElement;
-  video: HTMLVideoElement;
+  image: HTMLImageElement
+  text: HTMLParagraphElement
+  btn: HTMLButtonElement
+  video: HTMLVideoElement
 }
 interface ReadLess extends ReadMore {
-  minHeight: string;
+  minHeight: string
 }
 
-
 export function readMore({ image, text, btn, video }: ReadMore): void {
-  const imageRect = image.getBoundingClientRect();
-  const maxHeight = text.firstElementChild?.getBoundingClientRect().height;
+  const imageRect = image.getBoundingClientRect()
+  const maxHeight = text.firstElementChild?.getBoundingClientRect().height
 
   if (maxHeight !== undefined) {
     const setMargin = `-${imageRect.height + imageRect.y}px`
 
-    btn.setAttribute("data-isActive", "true");
+    btn.setAttribute('data-isActive', 'true')
 
     window.requestAnimationFrame(() => {
-      image.style.marginTop = setMargin;
-      video.style.marginBottom = setMargin;
-      text.classList.remove("line-clamp-3");
-      text.style.maxHeight = `${maxHeight}px`;
-    });
+      image.style.marginTop = setMargin
+      video.style.marginBottom = setMargin
+      text.classList.remove('line-clamp-3')
+      text.style.maxHeight = `${maxHeight}px`
+    })
 
-    btn.innerText = "Volver atrás";
+    btn.innerText = 'Volver atrás'
   }
 }
 
-export function readLess({ image, text, btn, video, minHeight }: ReadLess): void {
-  btn.removeAttribute("data-isActive");
+export function readLess({
+  image,
+  text,
+  btn,
+  video,
+  minHeight
+}: ReadLess): void {
+  btn.removeAttribute('data-isActive')
 
   window.requestAnimationFrame(() => {
-    image.style.marginTop = '0px';
-    video.style.marginBottom = '0px';
-    text.style.maxHeight = minHeight;
+    image.style.marginTop = '0px'
+    video.style.marginBottom = '0px'
+    text.style.maxHeight = minHeight
     setTimeout(() => {
-      text.classList.add("line-clamp-3");
-    }, 300);
-  });
+      text.classList.add('line-clamp-3')
+    }, 300)
+  })
 
-  btn.innerText = "Continuar leyendo";
+  btn.innerText = 'Continuar leyendo'
 }
